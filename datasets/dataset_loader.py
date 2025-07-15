a475304b3bb94232c68318967d436ac005a80615
Eric Baidoo <88762177+EricBaidoo@users.noreply.github.com>
2025-07-15 01:21:07 +0000

    Refactor GhanaFoodDataset class for improved structure and clarity in dataset loading

--- datasets/dataset_loader.py
+++ datasets/dataset_loader.py
@@ -1,41 +1,46 @@
-# Dataset loader for FRANI dataset
-
 import os
 import numpy as np
 from PIL import Image
 import torch
 from torch.utils.data import Dataset
-from torchvision import transforms
+import torchvision.transforms as T
+
 
 class GhanaFoodDataset(Dataset):
-    def __init__(self, root='data/frani/', split='train', transform=None):
-        self.root = root
-        self.split = split
-        self.transform = transform or transforms.Compose([
-            transforms.Resize((256, 256)),
-            transforms.ToTensor(),
+    def __init__(self, root_dir, transform=None, mask_transform=None):
+        """
+        root_dir: path to split (e.g., data/train, data/val, data/test)
+        Assumes images are in 'images/' and masks are in 'masks/' within root_dir.
+        """
+        self.image_dir = os.path.join(root_dir, 'images')
+        self.mask_dir = os.path.join(root_dir, 'masks')
+        self.image_files = sorted([
+            f for f in os.listdir(self.image_dir)
+            if f.endswith(('.jpg', '.png'))
         ])
-
-        self.image_dir = os.path.join(root, split, 'images')
-        self.mask_dir = os.path.join(root, split, 'masks')
-
-        self.images = sorted(os.listdir(self.image_dir))
-        self.masks = sorted(os.listdir(self.mask_dir))
+        self.transform = transform
+        self.mask_transform = mask_transform
 
     def __len__(self):
-        return len(self.images)
+        return len(self.image_files)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.image_dir, self.images[idx])
-        mask_path = os.path.join(self.mask_dir, self.masks[idx])
+        img_name = self.image_files[idx]
+        img_path = os.path.join(self.image_dir, img_name)
+        mask_name = img_name.replace('.jpg', '.png').replace('.jpeg', '.png')
+        mask_path = os.path.join(self.mask_dir, mask_name)
 
-        image = Image.open(img_path).convert('RGB')
-        mask = Image.open(mask_path).convert('L')  # grayscale for labels
+        image = Image.open(img_path).convert("RGB")
+        mask = Image.open(mask_path).convert("L")  # Grayscale mask with class IDs
 
         if self.transform:
             image = self.transform(image)
+        else:
+            image = T.ToTensor()(image)
 
-        mask = np.array(mask, dtype=np.int64)
-        mask = torch.tensor(mask, dtype=torch.long)
+        if self.mask_transform:
+            mask = self.mask_transform(mask)
+        else:
+            mask = torch.from_numpy(np.array(mask)).long()
 
         return image, mask
