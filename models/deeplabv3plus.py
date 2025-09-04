4f54ae3594373bec9ae3000ca0736ef2ff500c2c
Eric Baidoo <kbaidu7@gmail.com>
2025-09-04 18:06:11 +0000

    Remove BatchNorm from DeepLabV3+ global pool to fix training error

--- models/deeplabv3plus.py
+++ models/deeplabv3plus.py
@@ -29,7 +29,6 @@ class ASPP(nn.Module):
         self.global_pool = nn.Sequential(
             nn.AdaptiveAvgPool2d((1, 1)),
             nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
-            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
         )
 
